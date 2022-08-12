723e1049b8fbd0aca8b7d6898655a9e6f73a32f3
boy-mom <100622402+xoerycaxo@users.noreply.github.com>
2022-08-11 20:56:44 -0400

    added license logo

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-export function printMsg() {
-    console.log("This messege is from Eryca");
-}
+//packages needed for application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown.js');
+
+// array of questions for user
+const questions = [
+    // TODO: Include packages needed for this application
+    
+    ];
+    // TODO: Create an array of questions for user input
+    const questions = [];
+    
+    // function to write README file
+    function writeToFile(fileName, data) {
+    }
+    // TODO: Create a function to write README file
+    function writeToFile(fileName, data) {}
+    
+    // function to initialize program
+    function init() {
+    // TODO: Create a function to initialize app
+    function init() {}
+    
+    }
+    
+    // function call to initialize program
+    // Function call to initialize app
+    init();
+     
